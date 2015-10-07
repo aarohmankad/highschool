80749e5edb5b2d2ee745cdcbaf4012b18029deb9
Aaroh <aarohmankad@gmail.com>
2015-10-07 10:42:36 -0700

    added comments on queryingMongoose tutorial

--- computerScience/queryingMongoose/Student.js
+++ computerScience/queryingMongoose/Student.js
@@ -1,5 +1,8 @@
+// mongoose is what will let us connect to our database
 var mongoose = require('mongoose');
 
+// Create a Student model
+// Specify type of each model variable
 var Student = mongoose.model('Student', {
   id: String,
   name: String,
@@ -7,4 +10,7 @@ var Student = mongoose.model('Student', {
   gradeLevel: Number,
 });
 
+// Set Student as our module export.
+// Now other files can require this file 
+// and access the Student model and create students.
 module.exports = Student;
--- computerScience/queryingMongoose/server.js
+++ computerScience/queryingMongoose/server.js
@@ -1,8 +1,14 @@
+// mongoose is what will let us connect to our database
 var mongoose = require('mongoose');
+// our Student Object, check out Student.js
 var Student = require('./Student');
 
+// Use mongoose to connect to our database
 mongoose.connect('mongodb://localhost/school');
 
+// This is how you create a student
+// Notice how all the fields in Student.js are provided
+// If you don't specify a value, it will be null/false/0/''
 var aaroh = Student.create({
   id: '149003115',
   name: 'Aaroh',
@@ -10,10 +16,12 @@ var aaroh = Student.create({
   gradeLevel: 12,
 });
 
+// This is how we can access the data of any student
 aaroh.then(function (aarohData) {
   console.log(aarohData);
 });
 
+// Look familiar?
 var maxwell = Student.create({
   id: '148003878',
   name: 'Maxwell',
@@ -25,6 +33,8 @@ maxwell.then(function (maxwellData) {
   console.log(maxwellData);
 });
 
+// We can also find all student with 'name' of 'Aaroh'
+// Find another student using some other field
 Student.find({'name': 'Aaroh'}, function(err, student) {
   if (err) {
     console.log(err);

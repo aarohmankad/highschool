8aef6c1aece55334929872f805d2f0b7c8e8c3b6
Aaroh <aarohmankad@gmail.com>
2015-10-29 10:14:27 -0700

    update and delete students in client

--- computerScience/makingApi-client/js/app.js
+++ computerScience/makingApi-client/js/app.js
@@ -33,6 +33,58 @@ document.getElementById('studentForm').addEventListener('submit', function(event
 });
 
 /**
+ * when the update form is submitted, execute code to add student
+ * @param event {Object} event information on form submission
+ */
+document.getElementById('updateStudentForm').addEventListener('submit', function(event) {
+  // Prevent default action of submitting a form
+  event.preventDefault();
+
+  // Create a parameters object for sending to api
+  var params = {
+    name: document.getElementById('updateName').value,
+    age: parseInt(document.getElementById('updateAge').value),
+    gender: document.getElementById('updateGender').value,
+  };
+
+  // Send post request to api endpoint
+  request.send({
+    method: 'PUT',
+    url: 'http://localhost:8000/api/students/' + document.getElementById('updateIdentification').value,
+    data: params,
+  }, function(err, data) {
+    if (err) {
+      console.log(err);
+    }
+
+    // Print the data after we receive it
+    document.getElementById('getAllStudents').click();
+  });
+});
+
+/**
+ * when the delete form is submitted, execute code to add student
+ * @param event {Object} event information on form submission
+ */
+document.getElementById('deleteStudentForm').addEventListener('submit', function(event) {
+  // Prevent default action of submitting a form
+  event.preventDefault();
+
+  // Send post request to api endpoint
+  request.send({
+    method: 'DELETE',
+    url: 'http://localhost:8000/api/students/' + document.getElementById('deleteIdentification').value,
+  }, function(err, data) {
+    if (err) {
+      console.log(err);
+    }
+
+    // Print the data after we receive it
+    document.getElementById('getAllStudents').click();
+  });
+});
+
+/**
  * When the get students button is clicked, execute this code
  */
 document.getElementById('getAllStudents').addEventListener('click', function() {
@@ -52,12 +104,27 @@ document.getElementById('getAllStudents').addEventListener('click', function() {
 
 function addDataToHTML (data) {
   var row = document.createElement('tr');
-  row.innerHTML = data;
+  var nameCol = document.createElement('td');
+  var ageCol = document.createElement('td');
+  var idCol = document.createElement('td');
+  var genderCol = document.createElement('td');
+
+  nameCol.innerHTML = data.name;
+  ageCol.innerHTML = data.age;
+  idCol.innerHTML = data.id;
+  genderCol.innerHTML = data.gender;
+
+  row.appendChild(nameCol);
+  row.appendChild(ageCol);
+  row.appendChild(idCol);
+  row.appendChild(genderCol);
 
   document.getElementById('students').appendChild(row);
 }
 
 function replaceHTMLWithData (data) {
+  document.getElementById('students').innerHTML = '';
+
   for (var i = 0; i < data.length; i++) {
     var row = document.createElement('tr');
     var nameCol = document.createElement('td');
